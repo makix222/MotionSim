b960f17b19ee27fe9c1e6028b84e26608b6e6f59
Emerson <makix222@gmail.com>
2019-11-07 22:19:55 -0600

    Adjusted position of menu heading

--- menu.py
+++ menu.py
@@ -17,9 +17,10 @@ class Menu:
             desired_font = pygame.font.match_font('segoeui')
         else:
             desired_font = None
-        self.name_text = pygame.font.Font(desired_font, 4 * self.border_size)
-        self.name_text_screen = self.name_text.render(self.name, True, self.border_color)
-        self.name_location = (self.border_size, int(self.name_text_screen.get_size()[0] / 2))
+        self.font_size = 20
+        self.name_text = pygame.font.Font(desired_font, self.font_size)
+        self.name_text_screen = self.name_text.render(self.name, True, self.border_color, wp.background_color)
+        self.name_location = (int(self.name_text_screen.get_size()[0] / 2), 0)
 
     def draw_border(self):
         pygame.draw.rect(self.menu_screen, self.border_color, self.border_rect, self.border_size)
